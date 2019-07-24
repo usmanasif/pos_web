ee99196d28b1750f1bab9c3574d712679b80ec8c
MuneebDevs <muneebjavamob@gmail.com>
2019-07-24 13:19:45 +0500

    remove extra component

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Divider, Flag } from "semantic-ui-react";
-import SiteHeader from "./SiteHeader";
 const PageHeader = () => {
   return (
     <React.Fragment>
@@ -10,8 +9,6 @@ const PageHeader = () => {
         </b>
         POS by DEVSINC
       </h3>
-      <SiteHeader />
-
       <Divider />
     </React.Fragment>
   );

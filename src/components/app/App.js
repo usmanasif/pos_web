293ad8e474232fe68fbc41d10b32bfd02b0e77fc
MuneebDevs <muneebjavamob@gmail.com>
2019-07-25 20:15:27 +0500

    render home page when user is already signed in

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { connect } from "react-redux";
 import SignUp from "../authentication/signUp";
 import SignIn from "../authentication/signIn";
 import SignOut from "../authentication/signOut";
@@ -8,18 +9,19 @@ import Auth from "../authentication/auth";
 import Header from "../navbar/header";
 import Home from "../company/home";
 import "./App.css";
-function App() {
+
+function App({ isSignedIn }) {
   return (
     <Router>
       <div className="App">
         <Container className="marginTop" textAlign="justified">
           <Header />
           <Switch>
-            <Route path="/" exact component={Auth} />
             <Route path="/home" exact component={Home} />
             <Route path="/register" component={SignUp} />
             <Route path="/login" component={SignIn} />
             <Route path="/logout" component={SignOut} />
+            <Route path="/" exact component={isSignedIn ? Home : Auth} />
           </Switch>
         </Container>
       </div>
@@ -27,4 +29,9 @@ function App() {
   );
 }
 
-export default App;
+function mapStateToProps(state) {
+  const { isSignedIn } = state.reduxTokenAuth.currentUser;
+  return { isSignedIn };
+}
+
+export default connect(mapStateToProps)(App);

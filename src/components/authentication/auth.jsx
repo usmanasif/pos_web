acf3d048e596597e0c704d319a0fd3aa4c751ee8
MuneebDevs <muneebjavamob@gmail.com>
2019-07-25 20:13:14 +0500

    return null if user is loading

--- src/components/authentication/auth.jsx
+++ src/components/authentication/auth.jsx
@@ -1,39 +1,54 @@
-import React from "react";
+import React, { Component } from "react";
+import { connect } from "react-redux";
 import SignUp from "./signUp";
 import SignIn from "./signIn";
 import { Divider, Grid, Header, Icon, Segment, Label } from "semantic-ui-react";
-const Auth = props => {
-  return (
-    <Segment color="blue">
-      <Grid columns={2} relaxed="very">
-        <Divider vertical>Or</Divider>
-        <Grid.Row verticalAlign="middle">
-          <Grid.Column>
-            <Label ribbon color="blue"> First time here? </Label>
-            <Header>
-              <h1>
-                <Icon name="signup" />
-                Register
-              </h1>
-            </Header>
-            <SignUp {...props} />
-          </Grid.Column>
-          <Grid.Column>
-            <Label ribbon="right" color="blue">
-              Already have an Account?
-            </Label>
-            <Header>
-              <h1>
-                <Icon name="sign in" />
-                Login
-              </h1>
-            </Header>
-            <SignIn {...props} />
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
-    </Segment>
-  );
-};
 
-export default Auth;
+class Auth extends Component {
+  render() {
+    const { isLoading } = this.props;
+    if (isLoading) {
+      return null;
+    }
+    return (
+      <Segment color="blue">
+        <Grid columns={2} relaxed="very">
+          <Divider vertical>Or</Divider>
+          <Grid.Row verticalAlign="middle">
+            <Grid.Column>
+              <Label ribbon color="blue">
+                First time here?
+              </Label>
+              <Header>
+                <h1>
+                  <Icon name="signup" />
+                  Register
+                </h1>
+              </Header>
+              <SignUp {...this.props} />
+            </Grid.Column>
+            <Grid.Column>
+              <Label ribbon="right" color="blue">
+                Already have an Account?
+              </Label>
+              <Header>
+                <h1>
+                  <Icon name="sign in" />
+                  Login
+                </h1>
+              </Header>
+              <SignIn {...this.props} />
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Segment>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  const { isLoading } = state.reduxTokenAuth.currentUser;
+  return { isLoading };
+}
+
+export default connect(mapStateToProps)(Auth);

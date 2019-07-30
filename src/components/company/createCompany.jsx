394ded60fdfe1700dbc8fdfbeae1dc6b408c162b
Muneeb Ahmad <muneeb.ahmad@devsinc.com>
2019-07-30 13:31:07 +0500

    new reciept/bill page design

---
+++ src/components/company/createCompany.jsx
@@ -0,0 +1,82 @@
+import React, { Component } from "react";
+import Axios from 'axios';
+import {Form, Grid} from "semantic-ui-react";
+import ImageUpload from "../imageUploader/imageUpload"
+import {localUrl} from "../../utils/constants";
+
+class CreateCompany extends Component {    
+    constructor(props){
+        super(props)
+        this.state = {
+                name:'',
+                subdomain:'',
+                about_you:'',
+                logo:''
+        }
+    }
+
+    onChange = (e) => {
+        this.setState({ 
+            [e.target.name] : e.target.value
+        });
+    }
+
+    uploadImage = (url) => {
+        this.setState({logo: url})
+    }
+
+    createCompany = () =>{
+        console.log(this.state)
+        const {name, subdomain, about_you, logo} =this.state;
+        Axios.post(localUrl+"/companies",{name, subdomain, about_you, logo}) 
+            .then(function (response) {
+                // handle success
+                console.log(response);
+                // this.props.history.push("/home");
+            })
+            .catch(function (error) {
+                // handle error
+                console.log(error);
+            });
+    }
+
+    render() {
+        return (
+            <Grid centered>
+                <Grid.Row>
+                    <Grid.Column width={8}>
+                        <Form>
+                            <Form.Input 
+                                fluid label='Company name' 
+                                placeholder='Enter company name' 
+                                name="name"
+                                onChange={this.onChange}
+                                value={this.state.name}
+                            />
+                            <Form.Input 
+                                fluid label='Domain name' 
+                                placeholder='Enter domain name' 
+                                name="subdomain"
+                                onChange={this.onChange}
+                                value={this.state.subdomain}
+                            />
+                            <Form.TextArea 
+                                label='About' 
+                                placeholder='Tell us more about you...' 
+                                name="about_you"
+                                onChange={this.onChange}
+                                value={this.state.about_you}
+                            />
+                            <ImageUpload imageURL = {this.uploadImage}></ImageUpload>
+                            <Form.Button primary onClick={this.createCompany}>Register Company</Form.Button> 
+                        </Form>
+                    </Grid.Column>
+                </Grid.Row>
+            </Grid>
+        )
+    }
+
+}
+
+export default CreateCompany
+
--- src/components/company/home.jsx
+++ src/components/company/home.jsx
@@ -1,90 +1,12 @@
 import React, { Component } from 'react'
-<<<<<<< HEAD
-import Axios from 'axios';
-import {Form, Grid} from "semantic-ui-react";
-import ImageUpload from "../imageUploader/imageUpload"
-import {apiUrl} from "../../utils/api-config";
-=======
-import Navigation from './navigation'
 import NewReciept from './newReciept'
 
->>>>>>> new reciept/bill page design
 class Home extends Component {
-    constructor(props){
-        super(props)
-        this.state = {
-                name:'',
-                subdomain:'',
-                about_you:'',
-                logo:''
-        }
-    }
-
-    onChange = (e) => {
-        this.setState({
-            [e.target.name] : e.target.value
-         });
-    }
-
-    uploadImage = (url) => {
-        this.setState({logo: url})
-     }
-
-    createCompany = () =>{
-        console.log(this.state)
-        const {name, subdomain, about_you, logo} =this.state;
-        Axios.post(apiUrl+"/companies",{name, subdomain, about_you, logo})
-            .then(function (response) {
-                // handle success
-                console.log(response);
-                // this.props.history.push("/home");
-            })
-            .catch(function (error) {
-                // handle error
-                console.log(error);
-            });
-    }
-
     render() {
         return (
-<<<<<<< HEAD
-            <Grid centered>
-                <Grid.Row>
-                    <Grid.Column width={8}>
-                        <Form>
-                            <Form.Input
-                                fluid label='Company name'
-                                placeholder='Enter company name'
-                                name="name"
-                                onChange={this.onChange}
-                                value={this.state.name}
-                            />
-                            <Form.Input
-                                fluid label='Domain name'
-                                placeholder='Enter domain name'
-                                name="subdomain"
-                                onChange={this.onChange}
-                                value={this.state.subdomain}
-                            />
-                            <Form.TextArea
-                                label='About'
-                                placeholder='Tell us more about you...'
-                                name="about_you"
-                                onChange={this.onChange}
-                                value={this.state.about_you}
-                            />
-                            <ImageUpload imageURL = {this.uploadImage}></ImageUpload>
-                            <Form.Button primary onClick={this.createCompany}>Register Company</Form.Button>
-                        </Form>
-                    </Grid.Column>
-                </Grid.Row>
-            </Grid>
-=======
             <div>
-                <Navigation></Navigation>
                 <NewReciept></NewReciept>
             </div>
->>>>>>> new reciept/bill page design
         )
     }
 }

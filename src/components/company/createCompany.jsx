636814a5dee801271a6aafa4374e975a0bb47167
MuneebDevs <muneebjavamob@gmail.com>
2019-08-02 15:14:29 +0500

    create company through http service

--- src/components/company/createCompany.jsx
+++ src/components/company/createCompany.jsx
@@ -1,82 +1,82 @@
 import React, { Component } from "react";
-import Axios from 'axios';
-import {Form, Grid} from "semantic-ui-react";
-import ImageUpload from "../imageUploader/imageUpload"
-import {apiUrl} from "../../utils/api-config";
+import http from "../../services/httpService";
+import { Form, Grid } from "semantic-ui-react";
+import ImageUpload from "../imageUploader/imageUpload";
+import { apiUrl } from "../../utils/api-config";
+import { toast } from "react-toastify";
+class CreateCompany extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      subdomain: "",
+      about_you: "",
+      logo: ""
+    };
+  }
 
-class CreateCompany extends Component {    
-    constructor(props){
-        super(props)
-        this.state = {
-                name:'',
-                subdomain:'',
-                about_you:'',
-                logo:''
-        }
-    }
+  onChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
 
-    onChange = (e) => {
-        this.setState({ 
-            [e.target.name] : e.target.value
-        }); 
-    }
+  uploadImage = url => {
+    this.setState({ logo: url });
+  };
 
-    uploadImage = (url) => {
-        this.setState({logo: url})
-    }
+  createCompany = () => {
+    const { name, subdomain, about_you, logo } = this.state;
+    http
+      .post(apiUrl + "/companies", { name, subdomain, about_you, logo })
+      .then(response => {
+        console.log("this is response :", response);
+        toast(<h3>{response.data["name"]} Created Successfully</h3>);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
 
-    createCompany = () =>{
-        console.log(this.state)
-        const {name, subdomain, about_you, logo} =this.state;
-        Axios.post(apiUrl+"/companies",{name, subdomain, about_you, logo}) 
-            .then(function (response) {
-                // handle success
-                console.log(response);
-                // this.props.history.push("/home");
-            })
-            .catch(function (error) {
-                // handle error
-                console.log(error);
-            });
-    }
-
-    render() {
-        return (
-            <Grid centered>
-                <Grid.Row>
-                    <Grid.Column width={8}>
-                        <Form>
-                            <Form.Input 
-                                fluid label='Company name' 
-                                placeholder='Enter company name' 
-                                name="name"
-                                onChange={this.onChange}
-                                value={this.state.name}
-                            />
-                            <Form.Input 
-                                fluid label='Domain name' 
-                                placeholder='Enter domain name' 
-                                name="subdomain"
-                                onChange={this.onChange}
-                                value={this.state.subdomain}
-                            />
-                            <Form.TextArea 
-                                label='About' 
-                                placeholder='Tell us more about you...' 
-                                name="about_you"
-                                onChange={this.onChange}
-                                value={this.state.about_you}
-                            />
-                            <ImageUpload imageURL = {this.uploadImage}></ImageUpload>
-                            <Form.Button primary onClick={this.createCompany}>Register Company</Form.Button> 
-                        </Form>
-                    </Grid.Column>
-                </Grid.Row>
-            </Grid>
-        )
-    }
-
+  render() {
+    return (
+      <Grid textAlign="left">
+        <Grid.Row>
+          <Grid.Column width={8}>
+            <Form>
+              <Form.Input
+                fluid
+                label="Company name"
+                placeholder="Enter company name"
+                name="name"
+                onChange={this.onChange}
+                value={this.state.name}
+              />
+              <Form.Input
+                fluid
+                label="Domain name"
+                placeholder="Enter domain name"
+                name="subdomain"
+                onChange={this.onChange}
+                value={this.state.subdomain}
+              />
+              <Form.TextArea
+                label="About"
+                placeholder="Tell us more about you..."
+                name="about_you"
+                onChange={this.onChange}
+                value={this.state.about_you}
+              />
+              <ImageUpload imageURL={this.uploadImage} />
+              <Form.Button primary onClick={this.createCompany}>
+                Register Company
+              </Form.Button>
+            </Form>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    );
+  }
 }
 
-export default CreateCompany
-
+export default CreateCompany;

4b195507907671dc433c03ef36f1871a7ddc87ec
salahuldin <salah.din@devsinc.com>
2019-08-28 14:25:25 +0500

    per page items updated

--- src/components/inventory/inventory.jsx
+++ src/components/inventory/inventory.jsx
@@ -17,7 +17,7 @@ function searchingFor(item) {
 const initialPagination = {
   activePage: 1,
   totalPages: 0,
-  per_page: 4
+  per_page: 10
 }
 
 export default class Inventory extends Component {

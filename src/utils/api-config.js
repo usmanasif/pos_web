fadaea5cfc5b950d4f12bc1f430b3c6f7bf9a92c
Haseeb <abdulhaseeb.khn@gmail.com>
2019-08-30 15:57:35 +0500

    Fix back end url

--- src/utils/api-config.js
+++ src/utils/api-config.js
@@ -3,7 +3,7 @@ var arr = hostname.split(".");
 const subDomain = arr[0];
 var url = "";
 if (arr[1] === "herokuapp") {
-  url = "https://devsinc.herokuapp.com";
+  url = "https://devsinc-pos-api.herokuapp.com";
 } else if (
   (arr.length === 3 && arr[1] + "." + arr[2] === "lvh.me") ||
   (arr.length === 2 && arr[0] + "." + arr[1] === "lvh.me")

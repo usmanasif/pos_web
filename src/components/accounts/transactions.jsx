74b8e79263c846b62edc36bf798eb5ab238fe404
salahuldin <salah.din@devsinc.com>
2019-10-22 17:22:48 +0500

    transaction filters

--- src/components/accounts/transactions.jsx
+++ src/components/accounts/transactions.jsx
@@ -50,17 +50,6 @@ class Transections extends Component {
       .catch(error => console.log(error))
   };
 
-  getTransactions = () => {
-    http
-      .get(`${apiUrl}/api/v1/transactions`)
-      .then(res => {
-        this.setState({
-          Transactions: res.data.results[1]
-        });
-      })
-      .catch(error => console.log(error))
-  }
-
   getVendors = () =>{
     http
       .get(`${apiUrl}/api/v1/vendors`)
@@ -70,7 +59,19 @@ class Transections extends Component {
         });
       })
       .catch(error => console.log(error));
+  }
 
+  filterTransactions = (transactions) =>{
+    http
+      .get(`${apiUrl}/api/v1/transactions`, {params:transactions})
+      .then(res => {
+        console.log(res);
+        this.setState({
+          Transactions: res.data.results[1],
+          totalPages: res.data.results[0].total
+        });
+      })
+      .catch(error => console.log(error))
   }
 
   componentDidMount() {
@@ -82,7 +83,7 @@ class Transections extends Component {
     const {activePage, per_page, totalPages, Vendors, Transactions} = this.state;
     return (
       <React.Fragment>
-        <Filters users={Vendors}></Filters>
+        <Filters users={Vendors} filterTransactions={this.filterTransactions}></Filters>
         <Grid>
           <Grid.Column width={16}>
             <Button style={{ background: "#58ae61", color: "white" }} floated="right" onClick={this.redirect}><Icon name="plus"></Icon>New</Button>
